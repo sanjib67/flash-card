16e69a4ae402f537ff3ec6ae89a6ac170ba39969
Shivakumar Mangina <shiva2nani.mangina@gmail.com>
2020-06-08 15:56:48 +0530

    added tips and tricks

--- src/components/faq/TipsAndTricks.js
+++ src/components/faq/TipsAndTricks.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import graphOverview2 from '../../images/graph-overview-2.png';
-
-
+import tip1 from '../../images/tip1.png';
+import tip2 from '../../images/tip2.png';
+import tip3 from '../../images/tip3.png';
+import tip4 from '../../images/tip4.png';
+import tip5 from '../../images/tip5.png';
 
 export default class TipsAndTricks extends Component {
   render() {
     return (
       <div className='content'>
         <h1>Tips And Tricks</h1>
-         /**TODO : The below image is wrong Get images from Chinmoyee */
-        <img alt='' src={graphOverview2} />
-        <ol>
+        <ul>
           <li>
-            <strong>
-              <u>Header</u>:
-            </strong>{' '}
-            User can see the selected key parameters and latest time period
-            here.
+            <h2>Tip 1</h2>
+            <img alt='' src={tip1} />
           </li>
-          <li>Application version information.</li>
-        </ol>
+          <div class='dropdown-divider'></div>
+          <li>
+            <h2>Tip 2</h2>
+            <img alt='' src={tip2} />
+          </li>
+          <div class='dropdown-divider'></div>
+          <li>
+            <h2>Tip3</h2>
+
+            <img alt='' src={tip3} />
+          </li>
+          <div class='dropdown-divider'></div>
+          <li>
+            <h2>Tip 4</h2>
+
+            <img alt='' src={tip4} />
+          </li>
+          <div class='dropdown-divider'></div>
+          <li>
+            <h2>Tip 5</h2>
+
+            <img alt='' src={tip5} />
+          </li>
+        </ul>
       </div>
     );
   }
